dbaed1760127c91cb5a4d3a8312e490f3dc7d801
Trevor Drury <trevdrury@gmail.com>
2024-03-12 12:36:25 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/trevordrury/partpractice/PartPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.trevordrury.partpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevordrury/partpractice/PartPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevordrury.partpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
